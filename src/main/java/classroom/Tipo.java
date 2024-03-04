e4d5457b9ca151836ea4431b6f7e4ed584927c02
S3bastian2 <159493363+S3bastian2@users.noreply.github.com>
2024-03-04 15:26:41 -0500

    cambio decimoprimero en la clase Tipo

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,12 +1,16 @@
 package classroom;
 
 public enum Tipo {
-	DISCIPLINAR(10, ""), FUNDAMENTACION(20, ""), ELECTIVA(30,"");
+	DISCIPLINAR(10), FUNDAMENTACION(20), ELECTIVA(30);
 	
 	private int codigo;
 	private String nombre;
-	Tipo(int codigo, String nombre) {
+	private Tipo(int codigo, String nombre) {
 			this.codigo = codigo;
 			this.nombre = nombre;
+	}
+
+	private Tipo(int codigo) {
+		this(codigo, null);
 	}
 }
